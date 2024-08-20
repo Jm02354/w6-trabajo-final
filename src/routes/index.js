cfaeeeb00136f241ecc919a6e4e88c7128f9ec37
Jose Maria Chihuan <josemariabion@hotmail.com>
2024-08-19 22:43:18 -0500

    advance model user crud login

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const { route } = require('./user.router');
+const routerUser = require('./user.router');
 const router = express.Router();
 
-// colocar las rutas aquí
-
+// Put the routes here
+router.use('/users', routerUser)
 
 module.exports = router;
